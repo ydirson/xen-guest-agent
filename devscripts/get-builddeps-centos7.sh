7a427f255a4bb9ab28ba834b22b9ef79ad8eae83
Yann Dirson <yann.dirson@vates.fr>
2023-02-16 18:03:44 +0100

    First prototype with one-shot static os-release info

---
+++ devscripts/get-builddeps-centos7.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+yum install -y --enablerepo=epel cargo clang
+yum install -y xen-libs-devel
---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead};
+use xenstore_rs::{Xs, XsOpenFlags, XBTransaction};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let xs = Xs::new(XsOpenFlags::ReadOnly)?;
+
+    let os_info = collect_os()?;
+    publish_static(&xs, &os_info)?;
+
+    Ok(())
+}
+
+struct OsInfo {
+    pretty_name: String,
+    nickname: String,
+    version: String,
+}
+
+// /etc/os-release implementation
+fn collect_os() -> Result<OsInfo, io::Error> {
+    // arbitrary default values, should not happen
+    let mut nickname = "undefined-os".to_string();
+    let mut pretty_name = "Undefined Operating System".to_string();
+    let mut version = "".to_string();
+
+    let file = File::open("/etc/os-release")?;
+    for line_result in io::BufReader::new(file).lines() {
+        match line_result {
+            Ok(line) => { // FIXME not proper parsing of quoted shell string vars
+                let v: Vec<&str> = line.split("=").collect();
+                let (key, value) = (v[0], v[1]);
+                let value = value.trim_matches('"').to_string();
+                match key {
+                    "NAME" => nickname = value,
+                    "PRETTY_NAME" => pretty_name = value,
+                    "VERSION" => version = value,
+                    _ => (),
+                };
+            },
+            Err(err) => return Err(err), // FIXME propagation
+        }
+    }
+
+    let info = OsInfo {
+        pretty_name,
+        nickname,
+        version,
+    };
+
+    Ok(info)
+}
+
+// (partial) xenstore implementation with XenServer layout
+fn publish_static(xs: &Xs, os_info: &OsInfo) -> Result<(), io::Error> {
+    xs.write(XBTransaction::Null, "data/os_name", &os_info.pretty_name)?;
+    xs.write(XBTransaction::Null, "data/os_distro", &os_info.nickname)?;
+
+    Ok(())
+}
