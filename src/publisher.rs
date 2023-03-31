cc9cb67b0b0e0da5124128d3fc98f1bae2d7b7fc
Yann Dirson <yann.dirson@vates.fr>
2023-03-31 15:24:19 +0200

    Default publisher: print info when no concrete publisher is available
    This is intended for debugging collectors without polluting the xenstore,
    or for platorms without a publisher yet (which amounts to the same ;))

--- src/publisher.rs
+++ src/publisher.rs
@@ -1,5 +1,5 @@
 // default no-op Publisher implementation
-use crate::datastructs::{KernelInfo, NetEvent};
+use crate::datastructs::{KernelInfo, NetEvent, NetEventOp};
 use os_info;
 use std::error::Error;
 use std::io;
@@ -12,15 +12,33 @@ impl Publisher {
         Ok(Publisher {})
     }
 
-    pub fn publish_static(&self, os_info: &os_info::Info, _kernel_info: &Option<KernelInfo>,
-                          _mem_total_kb: Option<usize>,
+    pub fn publish_static(&self, os_info: &os_info::Info, kernel_info: &Option<KernelInfo>,
+                          mem_total_kb: Option<usize>,
     ) -> io::Result<()> {
+        println!("OS: {} - Version: {}", os_info.os_type(), os_info.version());
+        if let Some(mem_total_kb) = mem_total_kb {
+            println!("Total memory: {mem_total_kb} KB");
+        }
+        if let Some(KernelInfo{release}) = kernel_info {
+            println!("Kernel version: {}", release);
+        }
         Ok(())
     }
-    pub fn publish_memfree(&mut self, _mem_free_kb: usize) -> io::Result<()> {
+    pub fn publish_memfree(&mut self, mem_free_kb: usize) -> io::Result<()> {
+        println!("Free memory: {mem_free_kb} KB");
         Ok(())
     }
-    pub fn publish_netevent(&self, _event: &NetEvent) -> io::Result<()> {
+    pub fn publish_netevent(&self, event: &NetEvent) -> io::Result<()> {
+        let iface_id = &event.iface.name;
+        match &event.op {
+            NetEventOp::AddIp(address) => println!("{iface_id} +IP  {address}"),
+            NetEventOp::RmIp(address) => println!("{iface_id} -IP  {address}"),
+            NetEventOp::AddMac(mac_address) => println!("{iface_id} +MAC {mac_address}"),
+            NetEventOp::RmMac(mac_address) => println!("{iface_id} -MAC {mac_address}"),
+            _ =>
+                return Err(io::Error::new(io::ErrorKind::InvalidData,
+                                          "unhandled RtnlMessage: {nl_msg:?}")),
+        }
         Ok(())
     }
 }
